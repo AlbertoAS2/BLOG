3bc7423b0eedddccc738538768590cbbf9e2ee75
alberto <albertoahmedsanchez@gmail.com>
2024-10-04 15:45:42 +0200

    Crear nuevas rutas de enlaces.

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::view(uri: '/', view:'welcome');
+Route::view(uri: '/', view:'welcome')->name('home');
 
-Route::view(uri: 'contacto', view:'contact');
+Route::view(uri: 'contacto', view:'contact')->name('contact');
 
-Route::view(uri:'blog', view: 'blog');
+Route::view(uri:'blog', view: 'blog')->name('blog');
 
-Route::view(uri:'nosotros', view: 'about');
+Route::view(uri:'nosotros', view: 'about')->name('about');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
