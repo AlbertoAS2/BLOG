47a585e2e94a8cf63e31913a60148b00755c8d6f
alberto <albertoahmedsanchez@gmail.com>
2024-10-08 17:03:53 +0200

    Paso 7 -> Implementacion array desde base de datos

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,16 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+use function Laravel\Prompts\table;
+
 class PostController extends Controller
 {
     public function index()
     {
-        $post = [
-            ['title' => 'Post 1'],
-            ['title' => 'Post 2'],
-            ['title' => 'Post 3'],
-            ['title' => 'Post 4'],
-        ];
+        $post = DB::table(table: 'posts')-> get();
 
         return view('blog', compact(var_name: 'post'));
     }
